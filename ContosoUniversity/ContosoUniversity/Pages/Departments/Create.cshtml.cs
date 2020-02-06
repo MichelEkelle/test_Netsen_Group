81c8e5bb60f5cd6697f46b025cab4057b36dd07b
Michel EKELLE <ekelnguel@gmail.com>
2020-02-06 12:58:46 -0500

    ajout de l'exercice 1 (Tutoriel de 1-8)

---
+++ ContosoUniversity/ContosoUniversity/Pages/Departments/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Pages.Departments
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public CreateModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FirstMidName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Department Department { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Departments.Add(Department);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
